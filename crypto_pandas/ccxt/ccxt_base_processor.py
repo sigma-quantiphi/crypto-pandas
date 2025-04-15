cbe4cb6ba35cdb0323d520c259621329abf3c1e0
lucas <lucasjamar@hotmail.com>
2025-04-15 10:59:52 +0200

    remove filtering allowed columns

--- crypto_pandas/ccxt/ccxt_base_processor.py
+++ crypto_pandas/ccxt/ccxt_base_processor.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass, field
-from typing import Dict, Any
+from typing import Optional
 
 import pandera as pa
-from pandera.typing import Series
 
 from crypto_pandas.base_processor import BaseProcessor
 from crypto_pandas.order_schema import OrderSchema
@@ -11,13 +10,13 @@ from crypto_pandas.order_schema import OrderSchema
 class CCXTOrderSchema(pa.DataFrameModel):
     """Base schema for CCXT orders."""
 
-    id: str = pa.Field(nullable=True, default=None, required=False)
+    id: Optional[str] = pa.Field(nullable=True, default=None)
     symbol: str = pa.Field()
     side: str = pa.Field(isin=["buy", "sell"])
     type: str = pa.Field(isin=["limit", "market", "stop_loss", "take_profit"])
     amount: float = pa.Field(gt=0)
-    price: float = pa.Field(ge=0, nullable=True, default=None, required=False)
-    params: dict = pa.Field(nullable=True, default=None, required=False)
+    price: Optional[float] = pa.Field(ge=0, nullable=True, default=None)
+    params: Optional[dict] = pa.Field(nullable=True, default=None)
 
 
 @dataclass
--- crypto_pandas/order_schema.py
+++ crypto_pandas/order_schema.py
@@ -1,11 +1,13 @@
+from typing import Optional
+
 import pandera as pa
 
 
 class OrderSchema(pa.DataFrameModel):
     """Base schema for orders (general for any exchange)."""
 
-    id: str = pa.Field(nullable=True, default=None)
+    id: Optional[str] = pa.Field(nullable=True, default=None)
     symbol: str = pa.Field()
     side: str = pa.Field(isin=["BUY", "SELL"])
     type: str = pa.Field(isin=["LIMIT", "MARKET"])
-    price: float = pa.Field(ge=0, nullable=True, default=None)
+    price: Optional[float] = pa.Field(ge=0, nullable=True, default=None)
